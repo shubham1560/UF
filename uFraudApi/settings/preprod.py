186a40802b19795eaa100a06e5d4f2fa23607b8f
Bhubanesh Pinani <bhubaneshpinani@Bhubaneshs-MacBook-Air.local>
2020-06-06 14:16:50 +0530

    successful test with heroku

--- uFraudApi/settings/preprod.py
+++ uFraudApi/settings/preprod.py
@@ -1,1 +1,21 @@
 from .base import *
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'd1r2iam6t73l4b',
+
+        'USER': 'uaofyovxrlstew',
+
+        'PASSWORD': '207fe27a60a73ea6506282e4e165932738c4e3ff151eb5587a6980a1f7490723',
+
+        'HOST': 'ec2-54-86-170-8.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
+    }
+
+}
