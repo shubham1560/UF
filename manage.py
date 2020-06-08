e49e7b5d4f837a9c4f30ffbf366c1efc464a36a7
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-06-08 20:23:21 +0530

    For production

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uFraudApi.settings.dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uFraudApi.settings.preprod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- uFraudApi/wsgi.py
+++ uFraudApi/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uFraudApi.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uFraudApi.settings.preprod')
 
 application = get_wsgi_application()
 
