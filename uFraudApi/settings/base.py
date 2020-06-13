ba5e61fababbf9de638953a849596a93fb868415
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-06-13 21:16:11 +0530

    Added the email backend in the preproduction in base

--- uFraudApi/settings/base.py
+++ uFraudApi/settings/base.py
@@ -127,6 +127,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'avij1560@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = 'olabola12'
+
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
