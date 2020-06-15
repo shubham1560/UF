7bf85c031f51fcf889f0ed34c7d528768e44f102
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-06-15 20:39:14 +0530

    Serving staticfiles

--- uFraudApi/settings/base.py
+++ uFraudApi/settings/base.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
