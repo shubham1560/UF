ffcbd6f666c1653998384035c1853c2b8efaefa3
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-06-15 22:07:02 +0530

    Added the broker parameter to the redis url

--- uFraudApi/settings/dev.py
+++ uFraudApi/settings/dev.py
@@ -3,9 +3,9 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
-CELERY_BROKER_URL = 'redis://h:pc420dcd443a7edd61e683c8ef3e335d5f146d4ea54218667ab9297ae11ed0dca@ec2-52-23-127-211.compute-1.amazonaws.com:7419'
+# CELERY_BROKER_URL = 'redis://h:pc420dcd443a7edd61e683c8ef3e335d5f146d4ea54218667ab9297ae11ed0dca@ec2-52-23-127-211.compute-1.amazonaws.com:7419'
 
-#CELERY_BROKER_URL = "redis://rediscloud:pJUD3PHSRsTf0AK6luOVUTciPS8XMwgI@redis-17645.c14.us-east-1-2.ec2.cloud.redislabs.com:17645"
+# CELERY_BROKER_URL = "redis://rediscloud:pJUD3PHSRsTf0AK6luOVUTciPS8XMwgI@redis-17645.c14.us-east-1-2.ec2.cloud.redislabs.com:17645"
 
 DATABASES = {
 
