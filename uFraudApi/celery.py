d7e4e360e71a4b3b052b31c15f834156a7b38aa5
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-06-15 18:03:01 +0530

    Changed the celery.py file

--- uFraudApi/celery.py
+++ uFraudApi/celery.py
@@ -1,23 +1,26 @@
 from __future__ import absolute_import, unicode_literals
 from .settings import dev
+from .settings import preprod
 import os
 
 from celery import Celery
 from decouple import config
 # set the default Django settings module for the 'celery' program.
 
+app = Celery('uFraudApi')
+
 if config('LIVE') == '0':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uFraudApi.settings.dev')
+    app.autodiscover_tasks(lambda: dev.INSTALLED_APPS)
 else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uFraudApi.settings.finalsetup')
-
-app = Celery('uFraudApi')
+    app.autodiscover_tasks(lambda: preprod.INSTALLED_APPS)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.autodiscover_tasks(lambda: dev.INSTALLED_APPS)
+# app.autodiscover_tasks(lambda: dev.INSTALLED_APPS)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
