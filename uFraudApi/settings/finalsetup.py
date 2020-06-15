ca66664ce4786e964f46f6d65990a42fbc1c84f5
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-06-15 17:46:47 +0530

    Test 4

--- uFraudApi/settings/finalsetup.py
+++ uFraudApi/settings/finalsetup.py
@@ -16,9 +16,9 @@ if config('PRODUCTION') == '0':
     CACHES = {
         'default': {
             'BACKEND': 'redis_cache.RedisCache',
-            'LOCATION': '%s:%s' % ("ec2-52-23-127-211.compute-1.amazonaws.com", "7419"),
+            'LOCATION': '%s:%s' % ("redis-17645.c14.us-east-1-2.ec2.cloud.redislabs.com", "17645"),
             'OPTIONS': {
-                'PASSWORD': "pc420dcd443a7edd61e683c8ef3e335d5f146d4ea54218667ab9297ae11ed0dca",
+                'PASSWORD': "pJUD3PHSRsTf0AK6luOVUTciPS8XMwgI",
                 'DB': 0,
             }
         }
