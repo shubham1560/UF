51cede255aca3833ae4685ade337455664d8c310
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-06-25 23:07:52 +0530

    url cleaning

--- auth/urls.py
+++ auth/urls.py
@@ -1,10 +1,6 @@
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path, include
 from .views import UserViewSet, UserListViewSet, CreateUserViewSet, CreateGoogleUserViewSet, ActivateAccountViewSet, UserPasswordResetViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-# router.register('get_users', UserViewSet)
 
 urlpatterns = [
     path('get_token/', obtain_auth_token),
@@ -15,5 +11,4 @@ urlpatterns = [
     path('activate_account/<str:token>', ActivateAccountViewSet.as_view()),
     path('reset_password/<str:token>', UserPasswordResetViewSet.as_view()),
     path('rest-auth/', include('rest_auth.urls')),
-    path('', include(router.urls))
 ]
