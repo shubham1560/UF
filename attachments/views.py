4faa6c8e0ba195751b1061f075fb594fcd924e6d
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-07-06 02:48:54 +0530

    endpoint creation for upload of images

--- attachments/views.py
+++ attachments/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import AttachedImage
+# from rest_framework.views import APIView,
+from rest_framework import viewsets
+from rest_framework import serializers
+from django.http import HttpResponse
+
 
 # Create your views here.
+class AttachedImageViewSet(viewsets.ModelViewSet):
+    class AttachedImageSerializer(serializers.HyperlinkedModelSerializer):
+        class Meta:
+            model = AttachedImage
+            fields = ['id', 'article', 'real_image']
+
+    queryset = AttachedImage.objects.all()
+    serializers = AttachedImageSerializer
+
+    class AttachedImageSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = AttachedImage
+            fields = ('id', 'real_image', 'article')
+
+    def post(self, request, format=None):
+        image = request.data["image"]
+        upImage = AttachedImage.objects.create(real_image=image)
+        return HttpResponse({"message": upImage.real_image})
+
+
