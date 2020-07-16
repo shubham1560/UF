74be15b84b94d730a4be6aab67d46e10fa39c6cb
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-07-17 04:01:42 +0530

    Added image upload function

--- sys_user/urls.py
+++ sys_user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import GetUserDetailViewSet
+from .views import GetUserDetailViewSet, EditUserDetailViewSet, EditImageOnlyViewSet
 
 urlpatterns = [
     path('get_user_data/', GetUserDetailViewSet.as_view()),
+    path('edit_user_data/', EditUserDetailViewSet.as_view()),
+    path('edit_user_image/', EditImageOnlyViewSet.as_view()),
 ]
--- sys_user/views.py
+++ sys_user/views.py
@@ -3,13 +3,15 @@ from rest_framework.views import APIView
 from rest_framework import serializers, status
 from rest_framework.response import Response
 # from .services import remove_user
+from rest_framework.authtoken.models import Token
 
 
 class GetUserDetailViewSet(APIView):
     class GetUserDetailFromTokenSerializer(serializers.ModelSerializer):
         class Meta:
             model = SysUser
-            fields = ('id_name', 'first_name', 'last_name', 'profile_pic', 'profile', 'header_image')
+            fields = ('id_name', 'first_name', 'last_name', 'profile_pic', 'profile', 'header_image', 'email',
+                      'about', 'header_image')
 
     def get(self, request, format=None):
         serializer = self.GetUserDetailFromTokenSerializer(request.user, many=False)
@@ -22,3 +24,29 @@ class GetUserDetailViewSet(APIView):
             response = {"message": "User Deleted Successfully"}
             return Response(response, status=status.HTTP_200_OK)
 
+
+class EditUserDetailViewSet(APIView):
+
+    def post(self, request, format=None):
+        # breakpoint()
+        user = request.user
+        user.first_name = request.data['first_name']
+        user.last_name = request.data['last_name']
+        user.profile = request.data['profile']
+        user.about = request.data['about']
+        user.save()
+        return Response({"message": "changed"}, status=status.HTTP_200_OK)
+
+
+class EditImageOnlyViewSet(APIView):
+    class EditImageOnlySerializer(serializers.HyperlinkedModelSerializer):
+        class Meta:
+            model = SysUser
+            fields = ['profile']
+
+    def post(self, request, format=None):
+        # breakpoint()
+        user = Token.objects.get(key=request.data['token']).user
+        user.profile = request.data['profile']
+        user.save()
+        return Response({"message": "changed the image mofo"}, status=status.HTTP_200_OK)
