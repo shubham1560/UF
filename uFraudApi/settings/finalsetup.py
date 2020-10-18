b72a5264cc143c790586d9d04792506751036fb4
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-10-18 17:23:35 +0530

    Changed the database to premium

--- uFraudApi/settings/finalsetup.py
+++ uFraudApi/settings/finalsetup.py
@@ -55,6 +55,26 @@ else:
 
     CELERY_BROKER_URL = "redis://rediscloud:ok3AlmhlYZMSk7bQsfFhqri5c3d5dBJJ@redis-14029.c73.us-east-1-2.ec2." \
                         "cloud.redislabs.com:14029"
+    #
+    # DATABASES = {
+    #
+    #     'default': {
+    #
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #
+    #         'NAME': 'd5k6r7neqjv7cl',
+    #
+    #         'USER': 'jcwhtrhlznqqri',
+    #
+    #         'PASSWORD': '61c8859c27c769819ec7b91d2f932b248d12bfd047e2e932b8be55e79a3360aa',
+    #
+    #         'HOST': 'ec2-3-222-150-253.compute-1.amazonaws.com',
+    #
+    #         'PORT': '5432',
+    #
+    #     }
+    #
+    # }
 
     DATABASES = {
 
@@ -62,13 +82,13 @@ else:
 
             'ENGINE': 'django.db.backends.postgresql',
 
-            'NAME': 'd5k6r7neqjv7cl',
+            'NAME': 'd9c0nluu7dv5e8',
 
-            'USER': 'jcwhtrhlznqqri',
+            'USER': 'ieidkekrasxwji',
 
-            'PASSWORD': '61c8859c27c769819ec7b91d2f932b248d12bfd047e2e932b8be55e79a3360aa',
+            'PASSWORD': '4384cf08085469fe6501bf57bb4ea10a16d048a01fd07039dde636375aa6cc64',
 
-            'HOST': 'ec2-3-222-150-253.compute-1.amazonaws.com',
+            'HOST': 'ec2-54-164-134-207.compute-1.amazonaws.com',
 
             'PORT': '5432',
 
