ce853a878bd1a0e1995ff6f1adf75f449874ab12
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-11-17 18:51:17 +0530

    added change to the backend code

--- sys_user/services.py
+++ sys_user/services.py
@@ -50,64 +50,71 @@ def get_user_activity(request, requested_tye, start, end):
     # breakpoint()
     if not user.is_anonymous:
         if requested_tye == 'courses':
-            activities = KbUse.objects.filter(user=request.user,
-                                              article__isnull=True,
-                                              active=True).order_by('-sys_updated_on')[start:end]
+            try:
+                activities = KbUse.objects.filter(user=request.user,
+                                                  article__isnull=True,
+                                                  active=True).order_by('-sys_updated_on')[start:end]
+            except ObjectDoesNotExist:
+                return
             # activities = user.user_activity.select_related(
             #     'article',
             #     'course').exclude(course__isnull=True).order_by('-sys_updated_on')[start:end]
         if requested_tye == 'articles':
-            activities = KbUse.objects.filter(user=request.user,
-                                              percentage_completed__isnull=True,
-                                              active=True).order_by('-sys_updated_on')[start:end]
+            try:
+                activities = KbUse.objects.filter(user=request.user,
+                                                  percentage_completed__isnull=True,
+                                                  active=True).order_by('-sys_updated_on')[start:end]
+            except ObjectDoesNotExist:
+                return
             # activities = user.user_activity.select_related(
             #     'article',
             #     'course').exclude(article__isnull=True).order_by('-sys_updated_on')[start:end]
         # breakpoint()
         related_activities = [None]*len(activities)
         counter = 0
-        for activity in activities:
-            related_activities[counter] = {
-                "id": activity.id or '',
-                "viewed": activity.viewed or '',
-                "useful": activity.useful or '',
-                "progress": activity.percentage_completed or '',
-                "created_on": activity.sys_created_on or '',
-            }
-            if activity.course:
-                if activity.course.active:
-                    related_activities[counter]["course"] = {
-                        "id": activity.course.id or '',
-                        "label": activity.course.label or '',
-                        "description": activity.course.description or '',
-                        "compressed_image": config("S3URL") + str(activity.course.compressed_image) or '',
-                        "knowledge_base": activity.course.parent_kb_base.title or '',
-                        # "parent_kb_base": activity.course.parent_kb_base,
-                    }
-                else:
-                    related_activities = related_activities[1:]
-                    counter -= 1
+        if len(activities) > 0:
+            for activity in activities:
+                related_activities[counter] = {
+                    "id": activity.id or '',
+                    "viewed": activity.viewed or '',
+                    "useful": activity.useful or '',
+                    "progress": activity.percentage_completed or '',
+                    "created_on": activity.sys_created_on or '',
+                }
+                if activity.course:
+                    if activity.course.active:
+                        related_activities[counter]["course"] = {
+                            "id": activity.course.id or '',
+                            "label": activity.course.label or '',
+                            "description": activity.course.description or '',
+                            "compressed_image": config("S3URL") + str(activity.course.compressed_image) or '',
+                            "knowledge_base": activity.course.parent_kb_base.title or '',
+                            # "parent_kb_base": activity.course.parent_kb_base,
+                        }
+                    else:
+                        related_activities = related_activities[1:]
+                        counter -= 1
 
-            if activity.article:
-                if activity.article.active and activity.article.workflow == "published":
-                    related_activities[counter]["article"] = {
-                        "id": activity.article.id or '',
-                        "featured_image_thumbnail": config('S3URL')+str(activity.article.featured_image_thumbnail),
-                        "title": activity.article.title or '',
-                        "description": activity.article.description or '',
-                        "view_count": activity.article.view_count or '',
-                        "view_count_logged_in": activity.article.view_count_logged_in or '',
-                        "course_id": activity.article.section.course.id or '',
-                        "course_name": activity.article.section.course.label or '',
-                        "knowledge_base": activity.article.category.parent_kb_base.title or '',
-                        'article_url': activity.article.article_url or ''
-                        # "thumbnail": activity.article.
-                    }
-                else:
-                    related_activities = related_activities[1:]
-                    counter -= 1
-            counter += 1
-        # breakpoint()
+                if activity.article:
+                    if activity.article.active and activity.article.workflow == "published":
+                        related_activities[counter]["article"] = {
+                            "id": activity.article.id or '',
+                            "featured_image_thumbnail": config('S3URL')+str(activity.article.featured_image_thumbnail),
+                            "title": activity.article.title or '',
+                            "description": activity.article.description or '',
+                            "view_count": activity.article.view_count or '',
+                            "view_count_logged_in": activity.article.view_count_logged_in or '',
+                            "course_id": activity.article.section.course.id or '',
+                            "course_name": activity.article.section.course.label or '',
+                            "knowledge_base": activity.article.category.parent_kb_base.title or '',
+                            'article_url': activity.article.article_url or ''
+                            # "thumbnail": activity.article.
+                        }
+                    else:
+                        related_activities = related_activities[1:]
+                        counter -= 1
+                counter += 1
+            # breakpoint()
         # print(related_activities)
         # related_activities = []
         return list(related_activities)
